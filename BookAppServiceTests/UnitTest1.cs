94201e0ef9303c79f5dc0b567b981013e0eeeb4b
tavisca-mswarnesh <mswarnesh@tavisca.in>
2019-09-06 11:02:45 +0530

    Second Commit Changed naming conventions

--- BookAppServiceTests/UnitTest1.cs
+++ BookAppServiceTests/UnitTest1.cs
@@ -7,7 +7,68 @@ using Xunit;
 
 namespace BookAppServiceTests
 {
-    public class SevicesTest
+    public class PostTests
+    {
+        [Fact]
+        public void post_book_test()
+        {
+            BookServices bookServices = new BookServices();
+            List<Book> bookList = new List<Book>();
+            bookList.Add(new Book { Id = 1, Name = "Harry Potter", Price = 300, Author = "J K Rowling", Category = "Fiction" });
+
+            Assert.Equal("Added Successfully", bookServices.Post(bookList[0]).Message);
+            Assert.True(bookServices.Post(bookList[0]).Status);
+        }
+        [Theory]
+        [InlineData(-1, "Invalid Id")]
+        public void post_book_invalid_id_test(int id, string result)
+        {
+            BookServices bookServices = new BookServices();
+            List<Book> bookList = new List<Book>();
+            bookList.Add(new Book { Id = id, Name = "Harry Potter", Price = 300, Author = "J K Rowling", Category = "Fiction" });
+
+            Assert.Equal(result, bookServices.Post(bookList[0]).Message);
+            Assert.False(bookServices.Post(bookList[0]).Status);
+        }
+        [Theory]
+        [InlineData(-1, "Invalid Price")]
+        public void post_book_invalid_price_test(int price, string result)
+        {
+            BookServices bookServices = new BookServices();
+            List<Book> bookList = new List<Book>();
+            bookList.Add(new Book { Id = 1, Name = "Harry Potter", Price = price, Author = "J K Rowling", Category = "Fiction" });
+
+            Assert.Equal(result, bookServices.Post(bookList[0]).Message);
+            Assert.False(bookServices.Post(bookList[0]).Status);
+        }
+        [Theory]
+        [InlineData(",", "Author name only contains characters , spaces and .")]
+        [InlineData(",.", "Author name only contains characters , spaces and .")]
+        [InlineData("123", "Author name only contains characters , spaces and .")]
+
+        public void post_book_invalid_name_test(string name, string result)
+        {
+            BookServices bookServices = new BookServices();
+            List<Book> bookList = new List<Book>();
+            bookList.Add(new Book { Id = 1, Name = "Harry Potter", Price = 100, Author = name, Category = "Fiction" });
+
+            Assert.Equal(result, bookServices.Post(bookList[0]).Message);
+            Assert.False(bookServices.Post(bookList[0]).Status);
+        }
+        [Theory]
+        [InlineData("Abc", "Added Successfully")]
+        public void post_book_valid_name_test(string name, string result)
+        {
+            BookServices bookServices = new BookServices();
+            List<Book> bookList = new List<Book>();
+            bookList.Add(new Book { Id = 1, Name = "Harry Potter", Price = 100, Author = name, Category = "Fiction" });
+
+            Assert.Equal(result, bookServices.Post(bookList[0]).Message);
+            Assert.True(bookServices.Post(bookList[0]).Status);
+        }
+
+    }
+    public class GetTests
     {
         [Fact]
         public void get_empty_list_test()
@@ -49,63 +110,7 @@ namespace BookAppServiceTests
             BookServices bookServices = new BookServices();
             Assert.Equal(result, bookServices.Get(id).Message);
         }
-        [Fact]
-        public void post_book_test()
-        {
-            BookServices bookServices = new BookServices();
-            List<Book> bookList = new List<Book>();
-            bookList.Add(new Book { Id = 1, Name = "Harry Potter", Price = 300, Author = "J K Rowling", Category = "Fiction" });
-            
-            Assert.Equal("Added Successfully", bookServices.Post(bookList[0]).Message);
-            Assert.True(bookServices.Post(bookList[0]).Status);
-        }
-        [Theory]
-        [InlineData(-1, "Invalid Id")]
-        public void post_book_invalid_id_test(int id,string result)
-        {
-            BookServices bookServices = new BookServices();
-            List<Book> bookList = new List<Book>();
-            bookList.Add(new Book { Id = id, Name = "Harry Potter", Price = 300, Author = "J K Rowling", Category = "Fiction" });
-
-            Assert.Equal(result, bookServices.Post(bookList[0]).Message);
-            Assert.False(bookServices.Post(bookList[0]).Status);
-        }
-        [Theory]
-        [InlineData(-1, "Invalid Price")]
-        public void post_book_invalid_price_test(int price, string result)
-        {
-            BookServices bookServices = new BookServices();
-            List<Book> bookList = new List<Book>();
-            bookList.Add(new Book { Id = 1, Name = "Harry Potter", Price = price, Author = "J K Rowling", Category = "Fiction" });
-
-            Assert.Equal(result, bookServices.Post(bookList[0]).Message);
-            Assert.False(bookServices.Post(bookList[0]).Status);
-        }
-        [Theory]
-        [InlineData(",", "Author name only contains characters , spaces and .")]
-        [InlineData(",.", "Author name only contains characters , spaces and .")]
-        [InlineData("123", "Author name only contains characters , spaces and .")]
         
-        public void post_book_invalid_name_test(string name, string result)
-        {
-            BookServices bookServices = new BookServices();
-            List<Book> bookList = new List<Book>();
-            bookList.Add(new Book { Id = 1, Name = "Harry Potter", Price = 100, Author = name, Category = "Fiction" });
-
-            Assert.Equal(result, bookServices.Post(bookList[0]).Message);
-            Assert.False(bookServices.Post(bookList[0]).Status);
-        }
-        [Theory]
-        [InlineData("Abc", "Added Successfully")]
-        public void post_book_valid_name_test(string name, string result)
-        {
-            BookServices bookServices = new BookServices();
-            List<Book> bookList = new List<Book>();
-            bookList.Add(new Book { Id = 1, Name = "Harry Potter", Price = 100, Author = name, Category = "Fiction" });
-
-            Assert.Equal(result, bookServices.Post(bookList[0]).Message);
-            Assert.True(bookServices.Post(bookList[0]).Status);
-        }
 
     }
 }
